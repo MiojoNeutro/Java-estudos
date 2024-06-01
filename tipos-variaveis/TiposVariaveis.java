5b8cc7c7c7d4bdec9c2184f2a2e1e8a540d19ff9
Erick <ericksantos9991@gmail.com>
2024-05-31 21:30:51 -0300

    Codigo para juntar nomes

--- tipos-variaveis/TiposVariaveis.java
+++ tipos-variaveis/TiposVariaveis.java
@@ -1,6 +1,11 @@
 public class TiposVariaveis {
     
     public static void main(String[] args) {
-        
+        String primeiroNome = "Roberto";
+        String segundoNome = "Conor";
+        String ultimoNome = ultimoNome(primeiroNome, segundoNome);
+        System.out.println(ultimoNome);
+    }public static String ultimoNome(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
