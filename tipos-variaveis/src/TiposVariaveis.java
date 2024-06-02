5ca8d67103f14aa96ee355e65bc61d713a4607b0
Erick <ericksantos9991@gmail.com>
2024-06-02 17:42:03 -0300

    Resolvido bug da constante

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,8 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
         
-        
+        //Caso a váriavel não consiga conportar a quantidade de outra variável, acarretará em um erro.
+        //final: declara uma constante
+        //cast é usado para rodar o algoritimo mesmo assim.
 
+        short n1 = 1;
+        int numeroFinal = n1;
+        short n2 = (short) numeroFinal; 
+        System.out.println(n2);
+
+        final double VALOR_DE_PI = 3.1415;
+        System.out.println(VALOR_DE_PI);
     }
 
 }
